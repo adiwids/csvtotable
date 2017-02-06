6d5de2891d32aad092493aa16db72e4029b06983
Adi Widyawan <adi.widyawan@bocistudio.com>
2017-02-06 13:34:26 +0700

    Add mapping attribute, update README.

---
+++ README.md
@@ -0,0 +1,18 @@
+## Overview
+
+Library class to import CSV file into specific database table.
+
+## Development and Testing
+
+  - PHP 5.6.x
+  - PHPUnit
+  - MongoDB + extension (`mongodb-clients mongo-db-server libmongo-client0dev`)
+
+## License
+
+This code is licensed by MIT License. See license detail [here](LICENSE)
+
+## How to contribute?
+
+Please read [CONTRIBUTING.md](CONTRIBUTING.md) for detail!
+
--- test/csv_to_table_test.php
+++ test/csv_to_table_test.php
@@ -1,7 +1,21 @@
 <?php
 use PHPUnit\Framework\TestCase;
 
-class CsvToTableTest extends TestCase {
+$root = realpath(dirname(__FILE__)."/..");
+echo $root;
+require_once($root."/class.csv_to_table.php");
 
+class CsvToTableTest extends TestCase {
+  public function testInitializeWithInvalidArgument() {
+    $options = [
+      'source' => '../tmp/file.csv',
+      'connection' => [
+        'mongodb' => 'mongodb://localhost:12345'
+      ]
+    ];
+
+    $csvtotable = new CsvToTable($options);
+    $this->expectExceptionMessage("table argument should be defined");
+  }
 }
 ?>
